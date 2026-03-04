e0bc799db0cbf3bb1d0ea54cac12c6d317948dc3
safethecode <sam@nullk.us>
2026-03-04 20:05:01 +0900

    fix(client): shadow dangerous globals in getMDXExport for jsx

--- src/client/jsx.js
+++ src/client/jsx.js
@@ -1,3 +1,5 @@
+import {DANGEROUS_GLOBALS_SHADOW} from '../security.js'
+
 /**
  * @typedef {import('../types').MDXContentProps} MDXContentProps
  */
@@ -19,12 +21,15 @@ function getMDXComponent(code, jsxGlobals, globals) {
  * @template {{}} Frontmatter
  * @type {import('../types').MDXJsxExportFunction<ExportedObject, Frontmatter>}
  * @param {string} code - The string of code you got from bundleMDX
- * @param {Record<string, unknown>} jsxGlobals - JSX globals 
+ * @param {Record<string, unknown>} jsxGlobals - JSX globals
  * @param {Record<string, unknown>} [globals] - Any variables your MDX needs to have accessible when it runs
  *
  */
 function getMDXExport(code, jsxGlobals, globals) {
-  const scope = {...jsxGlobals, ...globals}
+  // Shadow dangerous globals so that common attack primitives (eval, process,
+  // require, etc.) are `undefined` inside the evaluated scope.  User-supplied
+  // globals can still override these if explicitly provided.
+  const scope = {...DANGEROUS_GLOBALS_SHADOW, ...jsxGlobals, ...globals}
   // eslint-disable-next-line
   const fn = new Function(...Object.keys(scope), code)
   return fn(...Object.values(scope))
